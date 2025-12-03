7d9f99b672c41a107f80b08878dec40ea8ca98f3
guardianprime <okongordian@gmail.com>
2025-12-03 09:18:40 +0100

    chore: delete and post routes with pagination for the get products route

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,18 +1,35 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import Product from "../models/productModel.js";
 
 const productRoutes = Router();
 
 //this is to get all the products in the database
-productRoutes.get("/", async (req, res) => {
+productRoutes.get("/", async (req: Request, res: Response) => {
   try {
-    res.json({ status: "success", data: "this is the project!!" });
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find().skip(skip).limit(limit);
+
+    const total = await Product.countDocuments();
+    const totalPages = Math.ceil(total / limit);
+
+    res.json({
+      status: "success",
+      page,
+      limit,
+      total,
+      totalPages,
+      count: products.length,
+      data: products,
+    });
     // const data = await ProductSchema;
   } catch (error) {}
 });
 
 // this is for adding new products
-productRoutes.post("/", async (req, res) => {
+productRoutes.post("/", async (req: Request, res: Response) => {
   try {
     const newProduct = new Product(req.body);
 
@@ -22,7 +39,7 @@ productRoutes.post("/", async (req, res) => {
       status: "success",
       data: newProduct,
     });
-  } catch (error) {
+  } catch (error: any) {
     res.status(400).json({
       status: "failed",
       message: error.message,
@@ -31,8 +48,9 @@ productRoutes.post("/", async (req, res) => {
 });
 
 //this is for updating products
-productRoutes.put("/:id", async (req, res) => {
+productRoutes.put("/:id", async (req: Request, res: Response) => {
   try {
+    await Product.findByIdAndUpdate(req.query.id);
     res.status(200).json({
       status: "success",
       message: "product successfully updated",
@@ -41,6 +59,21 @@ productRoutes.put("/:id", async (req, res) => {
     res.status(500).json({
       status: "failed",
       message: error,
+    });
+  }
+});
+
+productRoutes.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: "success",
+      message: `Product with ${req.params.id} deleted`,
+    });
+  } catch (error: any) {
+    res.status(405).json({
+      status: "failed",
+      message: error.message,
     });
   }
 });
